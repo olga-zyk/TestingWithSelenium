42035933817ae5d87c14d950fb1dd88b791a15de
Olga Zyk <my.agleh@gmail.com>
2019-02-14 09:08:35 +0200

    Rename UnitTest1 to TestClass; Add NUnit package; Add test method;

---
+++ TestingWithSelenium/TestClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TestingWithSelenium.PageObjects;
+using NUnit.Framework;
+
+namespace TestingWithSelenium
+{
+    [TestClass]
+    public class TestClass
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            HomePage home = new HomePage(_driver);
+            home.GoToPage();
+            CompanySelectionPage companySelection = home.GoToCompanySelectionPage();
+            companySelection.ChooseVilniusRegion();
+            companySelection.ChooseAllIndustries();
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (_driver != null)
+            {
+                _driver.Close();
+            }
+        }
+    }
+}
